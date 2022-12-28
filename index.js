cdc6a904557b0f5042c841ba686d0197d0ef2cb4
P-Lrou <98212750+P-Lrou@users.noreply.github.com>
2022-12-28 23:28:21 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ let galerieAll = document.querySelector('.galerieAll')
 let quisommenousAll = document.querySelector('.quisommenousAll')
 let téléchargementAll = document.querySelector('.téléchargementAll')
 let contactsAll = document.querySelector('.contactsAll')
+let cross = document.querySelector('.cross')
 
 
 /************DECLARATION ANIM***********/
@@ -38,7 +39,6 @@ accueil.forEach(elm => {
         galerieAll.style.display = "none"
         quisommenousAll.style.display = "none"
         téléchargementAll.style.display = "none"
-        contactsAll.style.display = "none"
         accueilAnim()
     })
 })
@@ -49,7 +49,6 @@ galerie.forEach(elm => {
         galerieAll.style.display = "block"
         quisommenousAll.style.display = "none"
         téléchargementAll.style.display = "none"
-        contactsAll.style.display = "none"
         galerieAnim()
     })
 })
@@ -60,7 +59,6 @@ quisommesnous.forEach(elm => {
         galerieAll.style.display = "none"
         quisommenousAll.style.display = "block"
         téléchargementAll.style.display = "none"
-        contactsAll.style.display = "none"
     })
 })
 
@@ -70,19 +68,57 @@ telechargements.forEach(elm => {
         galerieAll.style.display = "none"
         quisommenousAll.style.display = "none"
         téléchargementAll.style.display = "block"
-        contactsAll.style.display = "none"
     })
 })
 
 contacts.forEach(elm => {
     elm.addEventListener('click', () => {
-        accueilAll.style.display = "none"
-        galerieAll.style.display = "none"
-        quisommenousAll.style.display = "none"
-        téléchargementAll.style.display = "none"
-        contactsAll.style.display = "block"
+        document.querySelector(".modal").style.display = "block"
+        gsap.from(".modal", { x: 2100, duration: 0.5 });
     })
+})
+
+cross.addEventListener("click", () => {
+    gsap.to(".modal", { x: 2100, duration: 1 });
+    setTimeout(() => {
+        document.querySelector(".modal").style.display = "none"
+        gsap.to(".modal", { x: 0, duration: 1 });
+    }, 500);
 })
 
 headerAnim()
 accueilAnim()
+
+if (window.matchMedia("(width < 600px)").matches) {
+    document.querySelector('.video1').innerHTML = ''
+    document.querySelector('.video1').innerHTML = `<iframe style="width: 35vw; height: 18.5vw;" width="358.4" height="201.6"
+    src="https://www.youtube.com/embed/Ss6up4qf1Po" title="YouTube video player" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen></iframe>`
+    document.querySelector('.galerie_videos').innerHTML = ''
+    document.querySelector('.galerie_videos').innerHTML = `
+    <div class="video">
+        <iframe style="width: 35vw; height: 18.5vw;" width="358.4" height="201.6"
+            src="https://www.youtube.com/embed/1ApQzo7tydQ" title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+    </div>
+    <div class="video">
+        <iframe style="width: 35vw; height: 18.5vw;" width="358.4" height="201.6"
+            src="https://www.youtube.com/embed/yP9-p37ctyg" title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+    </div>
+    <div class="video">
+        <iframe style="width: 35vw; height: 18.5vw;" width="358.4" height="201.6"
+            src="https://www.youtube.com/embed/_fkYkO708pg" title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+    </div>
+    <div class="video">
+        <iframe style="width: 35vw; height: 18.5vw;" width="358.4" height="201.6"
+            src="https://www.youtube.com/embed/bcijASZfKVM" title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+    </div>`
+}
